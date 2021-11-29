812334147f17da262599f1227593baa75ed2d391
Thanathorn'Z <73480960+ThanathornZDev@users.noreply.github.com>
2021-11-29 10:01:06 +0700

    Add files via upload

---
+++ THZ-Script/fxmanifest.lua
@@ -0,0 +1,17 @@
+--[[
+	BASE SCRIPT FOR START
+]]
+fx_version 'adamant'
+game 'gta5'
+description 'THZ Develoepr!'
+version '1.2'
+
+client_scripts {
+	"config.lua",
+	"core/client.lua"
+}
+
+server_scripts {
+	"config.lua",
+	"core/server.lua"
+}
